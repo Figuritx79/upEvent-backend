154875f68cadd5cd12464414e2cdf9377fd93d04
Enrique Gonzalez <gonherenrique@gmail.com>
2025-02-13 23:35:16 -0600

    Lista, falta security

---
+++ src/main/java/mx/edu/utez/backendevent/BackendEventApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.backendevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/backendevent/BackendEventApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.backendevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
