b45cfd45676819fd6b5606c8733c89d2bff4fbe2
ViridianaPP <20233tn092@utez.edu.mx>
2025-03-25 21:53:12 -0600

    Generar QR y mandar email para confimacion de taller

---
+++ src/main/java/mx/edu/utez/backendevent/qr/service/QrGeneratorService.java
@@ -0,0 +1,34 @@
+package mx.edu.utez.backendevent.qr.service;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+import java.awt.image.BufferedImage;
+
+@Service
+public class QrGeneratorService {
+
+	public String generateQrBase64(String data) {
+		try {
+			QRCodeWriter qrCodeWriter = new QRCodeWriter();
+			BitMatrix bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, 250, 250);
+			BufferedImage qrImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
+
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			javax.imageio.ImageIO.write(qrImage, "png", baos);
+			byte[] qrBytes = baos.toByteArray();
+
+			return Base64.getEncoder().encodeToString(qrBytes);
+		} catch (WriterException | IOException e) {
+			throw new RuntimeException("Error al generar el QR", e);
+		}
+	}
+}
