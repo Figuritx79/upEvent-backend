14d08518a3c925af3f79cf19e922ded877b6f074
Antonio7347 <20233tn106@utez.edu.mx>
2025-03-25 23:20:42 -0600

    Avance en el modulo LandingPage

---
+++ src/main/java/mx/edu/utez/backendevent/landingPage/controller/LandingPageController.java
@@ -0,0 +1,46 @@
+package mx.edu.utez.backendevent.landingPage.controller;
+
+import jakarta.validation.Valid;
+import mx.edu.utez.backendevent.util.ResponseObject;
+import mx.edu.utez.backendevent.landingPage.model.LandingPageDto;
+import mx.edu.utez.backendevent.landingPage.service.LandingPageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/landing-page")
+public class LandingPageController {
+
+	private final LandingPageService service;
+
+	public LandingPageController(LandingPageService service) {
+		this.service = service;
+	}
+
+	@GetMapping("/landing-pages")
+	public ResponseEntity<ResponseObject> getAllLandingPages() {
+		return service.findAll();
+	}
+
+	@GetMapping("/landing-pages/{id}")
+	public ResponseEntity<ResponseObject> getLandingPageById(@PathVariable UUID id) {
+		return service.findById(id);
+	}
+
+	@PostMapping("/landing-page/create")
+	public ResponseEntity<ResponseObject> createLandingPage(@Valid @RequestBody LandingPageDto landingPageDto) {
+		return service.save(landingPageDto);
+	}
+
+	@PutMapping("/landing-page/update")
+	public ResponseEntity<ResponseObject> updateLandingPage(@Valid @RequestBody LandingPageDto landingPageDto) {
+		return service.update(landingPageDto);
+	}
+
+	@DeleteMapping("/landing-page/delete/{id}")
+	public ResponseEntity<ResponseObject> deleteLandingPage(@PathVariable UUID id) {
+		return service.deleteById(id);
+	}
+}
---
+++ src/main/java/mx/edu/utez/backendevent/landingPage/model/LandingPageRepository.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.backendevent.landingPage.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LandingPageRepository extends JpaRepository<LandingPage, UUID> {
+	boolean existsBySlug(String slug);
+	boolean existsByEventId(UUID eventId);
+	boolean existsBySlugAndIdNot(String slug, UUID id);
+}
